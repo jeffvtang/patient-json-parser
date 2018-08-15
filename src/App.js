4b761f4893ebf9800eb5f17266dc3b7dd1877b9f
Jeff <jeff.tang@live.com>
2018-08-15 03:36:44 -0400

    simple parser to required format w provided data

--- src/App.js
+++ src/App.js
@@ -3,19 +3,78 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      obj: {
+        "resourceType": "Patient",
+        "id": "xcda",
+        "text": {
+          "status": "generated",
+          "div": "\n      \n      <p>Henry Levin the 7th</>\n    \n    </div>"
+        },
+        "identifier": [
+          {
+            "use": "usual",
+            "type": {
+              "coding": [
+                {
+                  "system": "htt://hl7.org/fhir/v2/0203",
+                  "code": "MR"
+                }
+              ]
+            },
+            "system": "urn:oid:2.16.840.1.113883.19.5",
+            "value": "12345"
+          }
+        ],
+        "active": true,
+        "name": [
+          {
+            "family": [
+              "Levin"
+            ],
+            "given": [
+              "Henry"
+            ]
+          }
+        ],
+        "gender": "male",
+        "birthDate": "2002-09-24",
+        "managingOrganization": {
+          "reference": "Organization/2.16.840.1.113883.19.5",
+          "display": "University Health Network"
+        },
+        "conditions": [
+          "Diabetes",
+          "High blood pressure",
+          "Asthma"
+        ]
+      }
+    }
+  }
+
   render() {
+    console.log(this.state.obj)
+    const obj = this.state.obj
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+          <h1>Sample Patient Parser</h1>
+          <div>Name of patient: {obj.name[0].given + ' ' + obj.name[0].family}</div>
+          <div>Organization name: {obj.managingOrganization.display}</div>
+          <div>Gender: {obj.gender}</div>
+          <div>Number of conditions they have: {obj.conditions.length}</div>
+          <div>List of all conditions:</div>
+          <ul>
+          {obj.conditions.map((condition) => (
+          <li>{condition}</li>
+          ))}
+          </ul>
       </div>
     );
   }
 }
+
+
 
 export default App;
